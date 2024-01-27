8e3b0016f43a9d545d0573fdd70cbc9b8ced59a1
Akshaya HP <akshayhulkuli@gmail.com>
2024-01-27 18:32:39 +0530

    Saving changes

---
+++ src/components/RestaurantRecipe.js
@@ -0,0 +1,10 @@
+const RestaurantRecipe = (props) => {  
+    return (
+        <div className="res-recipe-accordian">
+            <h2>{props?.recipeInfo?.card?.card?.title}</h2>
+            
+        </div>
+    )
+}
+
+export default RestaurantRecipe;
--- src/components/RestuarantMenu.js
+++ src/components/RestuarantMenu.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { RESTAURANT_DATA_URL } from '../utils/constants'
+import RestaurantRecipe from './RestaurantRecipe'
 import Shimmer from './Shimmer'
 
 const RestaurantMenu = () => {
@@ -27,22 +28,31 @@ const RestaurantMenu = () => {
           'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
       )
     )
-    console.log(json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter(
-        card =>
-          card.card.card['@type'] ===
-          'type.googleapis.com/swiggy.presentation.food.v2.ItemCategory'
-      ))
   }
 
   return currentRestaurantData === null ? (
     <Shimmer />
   ) : (
     <div className='res-menu-page'>
-      <h1>{resInfo.name}</h1>
-      <h3>{resInfo.cuisines?.join(', ')}</h3>
-      <h3>Cost of two: {resInfo.costForTwo / 100}</h3>
-      <h2>Menu</h2>
-      <ul>
+      <div className='res-menu-res-info'>
+        <div className='res-menu-res-info-left'>
+            <h2 className='res-menu-res-info-header'>{resInfo.name}</h2>
+            <p>{resInfo.cuisines?.join(', ')}</p>
+            <p>{resInfo.areaName}, {resInfo.city}</p>
+            <p>{resInfo.feeDetails.message}</p>
+        </div>
+        <div className='res-menu-res-info-right'>
+            <p>&#11088; {"   " + resInfo.avgRating}</p>
+            <p className='rating-string'>{resInfo.totalRatingsString}</p>
+        </div>
+      </div>
+
+      <div>
+        {menuInfo.map(card => {
+            return <RestaurantRecipe recipeInfo={card} />
+        })}
+      </div>
+      {/* <ul>
         {menuInfo.map(card => (
           <li key={card?.card?.card?.title}>
             <h3>{card?.card?.card?.title}</h3>
@@ -58,7 +68,7 @@ const RestaurantMenu = () => {
             </ul>
           </li>
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
